091f0ad6ebee9efb40fe3ad1a00fcf588ccf7749
Marco Gaido <mgaido@fbk.eu>
2020-04-09 15:09:23 +0200

    [MULTI][BUG] Fix for loading of a model for inference

--- examples/speech_recognition/models/multilingual_conv_transformer.py
+++ examples/speech_recognition/models/multilingual_conv_transformer.py
@@ -127,6 +127,15 @@ class MultilingualConvolutionalTransformerModel(FairseqMultiModel):
         parser.add_argument('--share-decoders', action='store_true',
                             help='share decoders across languages')
 
+    def load_state_dict(self, state_dict, strict=True, args=None):
+        state_dict_subset = state_dict.copy()
+        for k, _ in state_dict.items():
+            assert k.startswith('models.')
+            lang_pair = k.split('.')[1]
+            if lang_pair not in self.models:
+                del state_dict_subset[k]
+        super().load_state_dict(state_dict_subset, strict=strict, args=args)
+
 
 class TokenWiseConvolutionalTransformerEncoder(ConvolutionalTransformerEncoder):
     """
